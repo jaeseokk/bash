2c7f37b13d19a9681f4cdc05889eeb6ea57e18fc
jaeseokk <kang89kr@gmail.com>
2023-12-08 16:12:22 +0900

    Fix header

--- packages/web/app/(other)/layout.tsx
+++ packages/web/app/(other)/layout.tsx
@@ -61,15 +61,8 @@ export default async function RootLayout({
               <div className="relative flex min-h-screen flex-col">
                 <header className="w-full bg-transparent">
                   <div className="container flex h-[5.25rem] items-center justify-between">
-                    <nav className="flex w-1/4 items-center space-x-6 text-sm font-medium">
-                      <Button
-                        variant="ghost"
-                        size="no-horizontal-padding"
-                        asChild
-                      >
-                        <Link href="/events/new">Create Event</Link>
-                      </Button>
-                    </nav>
+                    {/*<nav className="flex w-1/4 items-center space-x-6 text-sm font-medium"></nav>*/}
+                    <div className="w-1/4" />
                     <div className="flex w-1/2 items-center justify-center">
                       <Link href="/">
                         <LetsLogo />
