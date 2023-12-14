85b1b4e2930c6cf331a972b2855e367ce861af22
jaeseokk <kang89kr@gmail.com>
2023-12-14 17:48:15 +0900

    Enhance layer atom

--- packages/web/shared/stores/layer.ts
+++ packages/web/shared/stores/layer.ts
@@ -3,4 +3,16 @@ import { atomWithHash } from "jotai-location";
 export const layerAtom = atomWithHash<string | undefined>("layer", undefined, {
   serialize: (value) => (value ? value : ""),
   deserialize: (value) => value,
+  setHash: (searchParams) => {
+    const value = searchParams.split("=")[1];
+    if (value) {
+      window.location.hash = searchParams;
+    } else {
+      window.history.replaceState(
+        null,
+        "",
+        `${window.location.pathname}${window.location.search}`,
+      );
+    }
+  },
 });
